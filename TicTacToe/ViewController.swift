c6703bcb1aaaa66069a0f9a0675a6b0446c42ca3
mikepitre <michaelepitre@gmail.com>
2015-09-24 14:31:24 -0500

    added logic to check for a winner in any combination

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -13,33 +13,53 @@ class ViewController: UIViewController {
     //1 = Os, 2 = Xs
     
     var activePlayer = 1
+    
+    var gameState = [0, 0, 0 , 0, 0, 0, 0, 0, 0]
+    
+    var winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
     @IBOutlet weak var button: UIButton!
     
     
     @IBAction func buttonPressed(sender: AnyObject) {
         
-        var image = UIImage()
+        if gameState[sender.tag] == 0 {
         
-        if activePlayer == 1 {
+            var image = UIImage()
+        
+            gameState[sender.tag] = activePlayer
+        
+            if activePlayer == 1 {
           
-            image = UIImage(named: "o.png")!
+                image = UIImage(named: "o.png")!
             
-            activePlayer = 2
+                activePlayer = 2
             
-        } else {
+            } else {
             
-            if activePlayer == 2 {
+                if activePlayer == 2 {
                 
-                image = UIImage(named: "x.png")!
+                    image = UIImage(named: "x.png")!
                 
-                activePlayer = 1
+                    activePlayer = 1
                 
+                }
+        
             }
         
+            sender.setImage(image, forState: .Normal)
+            
+            for combination in winningCombinations {
+            
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                
+                    print("We have a winner!")
+                
+                }
+                
+            }
+            
         }
-        
-        sender.setImage(image, forState: .Normal)
         
     }
     
